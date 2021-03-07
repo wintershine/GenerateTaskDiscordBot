e515d16a7c67376ed6e074aa83edc0870bce0a80
GUILLAUME\Ghivert <Guillaume.Hivert@usherbrooke.ca>
2021-03-06 20:27:11 -0500

    Add slash commands Add ability to delete and modify accounts

--- init.py
+++ init.py
@@ -1,23 +1,29 @@
 import discord
+from discord_slash import SlashCommand
+from discord_slash.utils import manage_commands
 from time import time
 from asyncio import sleep
 from discord.ext import tasks
+from discord.ext import commands
 import googleapiclient.errors
 
 import gsheet
 from taskAccount import *
 from taskAccountList import taskAccountList
 
-client = discord.Client()
+client = commands.Bot(intents = discord.Intents.default(), command_prefix='&')
+slash = SlashCommand(client, sync_commands=True)
 channelName = 'bot-command'
 
 sheet = gsheet.gsheet()
 
 maxChecksPerCycle = 50
-maxUpdatesPerCycle = 10
+maxUpdatesPerCycle = 5
 taskAccountList = taskAccountList()
 
-@tasks.loop(minutes=2)
+guild_ids = [594378237417881600, 569436224457146388]
+
+@tasks.loop(minutes=5)
 async def updateTaskAccounts():
     taskAccounts = taskAccountList.taskAccounts
     print('Starting periodic update...')
@@ -48,151 +54,216 @@ async def on_ready():
     if(not updateTaskAccounts.get_task()):
         updateTaskAccounts.start()
 
-@client.event
-async def on_message(message):
-    if message.author == client.user or not message.content.startswith('!') or message.channel.name != channelName:
+@slash.slash(
+    name = "add",
+    description = "Add a new account to the leaderboards",
+    options = [manage_commands.create_option(
+        name = "accountName",
+        description = "Name of the account",
+        option_type = 3,
+        required = True
+    ),
+    manage_commands.create_option(
+        name = "isTaskAccount",
+        description = "Is the account an official task account?",
+        option_type = 5,
+        required = True
+    ),
+        manage_commands.create_option(
+        name = "spreadsheetUrl",
+        description = "Link to the account's generatetask spreadsheet",
+        option_type = 3,
+        required = True
+    )],
+    guild_ids = guild_ids
+)
+async def _add(ctx, accountName: str, isTaskAccount: bool, spreadsheetUrl: str):
+    await ctx.respond()
+
+    response = ""
+    if(getTaskAccountFromNickname(accountName)):
+        response = f'Account {accountName} already exists'
+        await ctx.send(response)
         return
 
-    msg = message.content[1:]
-    result = [x.strip() for x in msg.split(' ')]
-    command = result[0]
+    newTaskAccount = taskAccount(spreadsheetUrl, accountName, isTaskAccount)
+   
+    try:
+        sheet.updateTaskAccount(newTaskAccount)
+        taskAccountList.add(newTaskAccount)
+        taskAccountList.updateLastUpdated(newTaskAccount, time())
+        response = f'Added account "{accountName}"' 
+    except googleapiclient.errors.HttpError as err:
+        if '403' in str(err.content):
+            msg = 'The bot does not have viewing rights to your sheet. Please make your sheet visible to the public or give view rights to '
+            msg = msg + '"generatetaskleaderboards@quickstart-1561791519322.iam.gserviceaccount.com"'
+            response = msg
+        else:
+            response = 'An error happened while adding your sheet, try again later or contact Sarasun#3674 (Torgasun) on discord for support'
+            print(err)
 
-    if(command == 'add'):
-        await add(result, False, message)
+    await ctx.send(response)
+
+@slash.slash(
+    name = "modifyAccount",
+    description = "Change an account's name, taskAccount status or spreadsheet link",
+    options = [manage_commands.create_option(
+        name = "currentAccountName",
+        description = "Name of the account currently",
+        option_type = 3,
+        required = True
+    ),
+    manage_commands.create_option(
+        name = "newAccountName",
+        description = "Name you want to change the account to",
+        option_type = 3,
+        required = True
+    ),
+    manage_commands.create_option(
+        name = "isTaskAccount",
+        description = "Is the account an official task account?",
+        option_type = 5,
+        required = True
+    ),
+    manage_commands.create_option(
+        name = "spreadsheetUrl",
+        description = "Link to the new spreadsheet",
+        option_type = 3,
+        required = True
+    )],
+    guild_ids = guild_ids
+)
+async def _modifyAccount(ctx, oldAccountName: str, newAccountName: str, isTaskAccount: bool, spreadsheetUrl: str):
+    await ctx.respond()
+
+    taskAccount = getTaskAccountFromNickname(oldAccountName)
+    response = ""
+    if(taskAccount):
+        taskAccount.isOfficial = isTaskAccount
+        taskAccount.spreadsheetUrl = spreadsheetUrl
+        taskAccount.nickname = newAccountName
+    else:
+        response = f'Account {oldAccountName} does not exist'
+        await ctx.send(response)
+        return
+
+    try:
+        sheet.deleteAccount(oldAccountName)
+        sheet.updateTaskAccount(taskAccount)
+        taskAccountList.updateLastUpdated(taskAccount, time())
+        response = f'Modified account "{oldAccountName}"' 
+    except googleapiclient.errors.HttpError as err:
+        if '403' in str(err.content):
+            msg = 'The bot does not have viewing rights to your new sheet. Please make it visible to the public or give view rights to '
+            msg = msg + '"generatetaskleaderboards@quickstart-1561791519322.iam.gserviceaccount.com"'
+            response = msg
+        else:
+            response = 'An error happened while modifying your sheet, try again later or contact Sarasun#3674 (Torgasun) on discord for support'
+            print(err)
+
+    await ctx.send(response)
+
+@slash.slash(
+    name = "deleteAccount",
+    description = "Removes an account from the leaderboards",
+    options = [manage_commands.create_option(
+        name = "accountName",
+        description = "Name of the account",
+        option_type = 3,
+        required = True)],
+    guild_ids = guild_ids
+)
+async def _deleteAccount(ctx, accountName: str):
+    await ctx.respond()
+
+    taskAccount = getTaskAccountFromNickname(accountName)
+    if(taskAccount):
+        sheet.deleteAccount(accountName)
+        taskAccountList.remove(taskAccount)
+        await ctx.send(f'Account "{accountName}" was removed from the leaderboards')
+    else:
+        await ctx.send(f'Account "{accountName}" does not exist')
+        return
+
+@slash.slash(
+    name = "update",
+    description = "Updates the leaderboards for an account",
+    options = [manage_commands.create_option(
+        name = "accountName",
+        description = "Name of the account",
+        option_type = 3,
+        required = True
+    )],
+    guild_ids = guild_ids
+)
+async def _update(ctx, accountName: str):
+    await ctx.respond()
+
+    response = ""
+    taskAccountToUpdate = getTaskAccountFromNickname(accountName)
+    if not taskAccountToUpdate:
+        response = f'There is no registered account with the nickname "{accountName}"!'
+        await ctx.send(response)
+        return
+    sheet.updateTaskAccount(taskAccountToUpdate)
+    taskAccountList.updateLastUpdated(taskAccountToUpdate, time())
+    response = f'Updated account "{accountName}"\n'
+    response = response + taskAccountToUpdate.getAccountInfo()
+
+    await ctx.send(response)
+
+@slash.slash(
+    name = "latest",
+    description = "Updates a user's spreadsheet to the latest version",
+    options = [manage_commands.create_option(
+        name = "accountName",
+        description = "Name of the account",
+        option_type = 3,
+        required = True
+    )],
+    guild_ids = guild_ids
+)
+async def _latest(ctx, accountName: str):
+    await ctx.respond()
+
+    response = ""
+    try:
+        taskAccountToUpdate = getTaskAccountFromNickname(accountName)
+        if taskAccountToUpdate is None:
+            response = f'There is no registered account with the nickname "{accountName}"!'
+            await ctx.send(response)
+            return
+        remainingTasks = sheet.updateSpreadsheetToLatestVersion(taskAccountToUpdate.spreadsheetUrl)
+
+        response = 'Your sheet was updated to the latest version!\n'
+        if(remainingTasks):
+            remainingTasksWarning = 'These tasks could not be filled into the new sheet. This means they got changed, renamed or removed:\n'
+            for remainingTask, count in remainingTasks.items():
+                remainingTasksWarning = remainingTasksWarning + f'{remainingTask} - {count} time(s)\n' 
+            remainingTasksWarning = remainingTasksWarning + 'You will have to manually fill them in if applicable\n'
+            response = response + remainingTasksWarning
+        response = response + 'Remember that if there were changes to the tier you are currently on, it is possible you might have to change the hidden "info" tab'
+    except googleapiclient.errors.HttpError as err:
+        if '403' in str(err.content):
+            response = 'The bot does not have edit rights to your sheet. Please give edit rights to "generatetaskleaderboards@quickstart-1561791519322.iam.gserviceaccount.com" '
+            response = response + 'if you want it to update your sheet to the latest version.'
     
-    elif(command == 'addofficial'):
-        await add(result, True, message)
-        
-    elif(command == 'update'):
-        await update(result, message)
+    await ctx.send(response)
 
-    elif(command == 'latest'):
-        await latest(result, message)
-
-    elif(command == 'info'):
-        await info(result, message)
-        
-    elif(command == 'leaderboard' or command == 'leaderboards'):
-       await leaderboard(message)
-
-    elif (command == 'help' or command == 'h' or command == 'commands' or command == 'options'):
-        await help(result, message)
+@slash.slash(
+    name = "leaderboards",
+    description = "Posts a link to the leaderboards",
+    guild_ids = guild_ids
+)
+async def _leaderboards(ctx):
+    await ctx.respond()
+    await ctx.send('The leaderboards can be found at https://docs.google.com/spreadsheets/d/1Pb4p4qFPaJ2nA7ABwxVzazF2pYDiIVpoGHH-BolKtJY')
 
 def getTaskAccountFromNickname(nickname):
     for taskAccount in taskAccountList.taskAccounts:
         if(taskAccount.nickname == nickname):
             return taskAccount
     return None
-
-# botCommands Functions
-async def add(result, isOfficial, message):
-    if(len(result) < 3):
-        await message.channel.send('Error: You need to add accounts with the syntax !add [[spreadsheetUrl]] [[nickname]]')
-        return
-    spreadsheetUrl = result[1]
-    nickname = result[2]
-    for res in result[3:]:
-        nickname += ' ' + res
-    
-    if(getTaskAccountFromNickname(nickname)):
-        await message.channel.send(f'Account {nickname} already exists')
-        return
-
-    newTaskAccount = taskAccount(spreadsheetUrl, nickname, isOfficial)
-   
-    try:
-        sheet.updateTaskAccount(newTaskAccount)
-        taskAccountList.add(newTaskAccount)
-        taskAccountList.updateLastUpdated(newTaskAccount, time())
-        await message.channel.send(f'Added account "{nickname}"')
-    except googleapiclient.errors.HttpError as err:
-        if '403' in str(err.content):
-            msg = 'The bot does not have viewing rights to your sheet. Please make your sheet visible to the public or give view rights to '
-            msg = msg + '"generatetaskleaderboards@quickstart-1561791519322.iam.gserviceaccount.com"'
-            await message.channel.send(msg)
-        else:
-            await message.channel.send('An error happened while adding your sheet, try again later')
-            print(err)
-    
-
-async def update(result, message):
-    if(len(result) < 2):
-        await message.channel.send('Error: You need to update accounts with the syntax !update [[nickname]]')
-    nickname = result[1]
-    for res in result[2:]:
-        nickname += ' ' + res
-    taskAccountToUpdate = getTaskAccountFromNickname(nickname)
-    if not taskAccountToUpdate:
-        await message.channel.send(f'There is no registered account with the nickname "{nickname}"!')
-        return
-    sheet.updateTaskAccount(taskAccountToUpdate)
-    taskAccountList.updateLastUpdated(taskAccountToUpdate, time())
-    await message.channel.send(f'Updated account "{nickname}"')
-    await message.channel.send(taskAccountToUpdate.getAccountInfo())
-
-async def latest(result, message):
-    try:
-        if(len(result) < 2):
-            await message.channel.send('Error: You need to update to the latest version with the syntax !latest [[nickname]]')
-        nickname = result[1]
-        for res in result[2:]:
-            nickname += ' ' + res
-        taskAccountToUpdate = getTaskAccountFromNickname(nickname)
-        if not taskAccountToUpdate:
-            await message.channel.send(f'There is no registered account with the nickname "{nickname}"!')
-            return
-        remainingTasks = sheet.updateSpreadsheetToLatestVersion(taskAccountToUpdate.spreadsheetUrl)
-
-        await message.channel.send('Your sheet was updated to the latest version!')
-        if(remainingTasks):
-            remainingTasksWarning = 'These tasks could not be filled into the new sheet. This means they got changed, renamed or removed:\n'
-            for remainingTask, count in remainingTasks.items():
-                remainingTasksWarning = remainingTasksWarning + f'{remainingTask} - {count} time(s)\n' 
-            await message.channel.send(remainingTasksWarning)
-            remainingTasksWarning = remainingTasksWarning + 'You will have to manually fill them in if applicable'
-        await message.channel.send('Remember that if there were changes to the tier you are currently on, it is possible you might have to change the hidden "info" tab')
-    except googleapiclient.errors.HttpError as err:
-        if '403' in str(err.content):
-            msg = 'The bot does not have edit rights to your sheet. Please give edit rights to "generatetaskleaderboards@quickstart-1561791519322.iam.gserviceaccount.com" '
-            msg = msg + 'if you want it to update your sheet to the latest version.'
-            await message.channel.send(msg)
-
-async def info(result, message):
-    nickname = result[1]
-    for res in result[2:]:
-        nickname += ' ' + res
-    taskAccountToGetInfoFrom = getTaskAccountFromNickname(nickname)
-    if not taskAccountToGetInfoFrom:
-        await message.channel.send(f'There is no registered account with the nickname "{nickname}"!')
-        return
-    await message.channel.send(taskAccountToGetInfoFrom.getAccountInfo())
-    
-async def leaderboard(message):
-     await message.channel.send('The leaderboards can be found at https://docs.google.com/spreadsheets/d/1Pb4p4qFPaJ2nA7ABwxVzazF2pYDiIVpoGHH-BolKtJY')
-        
-async def help(result,message):
-    if(len(result) == 1):
-            await message.channel.send('Available commands: !add, !addofficial !update, !info, !leaderboards, !help,')
-    elif(len(result) == 2):
-        if(result[1] == 'add'):
-            await message.channel.send('"add" allows you to add your account to the leaderboards! \n\
-                Syntax: !add [[spreadsheetUrl]] [[nickname]]. I recommend that you set your nickname as your RS name \
-                but it is not required'.replace("                        ",""))
-        elif(result[1] == 'addofficial'):
-            await message.channel.send('"addofficial" allows you to add your official taskaccount to the leaderboards! \n\
-                Syntax: !addofficial [[spreadsheetUrl]] [[nickname]]. I recommend that you set your nickname as your RS name \
-                but it is not required'.replace("                        ",""))
-        elif(result[1] == 'update'):
-            await message.channel.send('"update" will force an update on the leaderboards for your account \n\
-                Syntax: !update [[nickname]]'.replace("                        ",""))
-        elif(result[1] == 'info'):
-            await message.channel.send('"info" will give you information about a specific task account \n\
-                Syntax: !info [[nickname]]'.replace("                        ",""))
-        elif(result[1] == 'leaderboards'):
-            await message.channel.send('"leaderboards" will provide a link to the leaderboards \n\
-                Syntax: !info [[nickname]]'.replace("                        ",""))
-    else:
-        await(message.channel.send('Unrecognized help command'))
 
 discordBotToken = ''
 with open('discordbot_token.txt', 'r') as token:
